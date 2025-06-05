8f78f651a4bdc5dad8d77f3da5705cd9b65fc751
jose781 <146405410+jfernando234@users.noreply.github.com>
2025-06-05 18:21:10 -0500

    Inicio el proyecto parte del back con spring boot

---
+++ src/main/java/com/example/sbootporlles/SbootporllesApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbootporlles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootporllesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootporllesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbootporlles/SbootporllesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbootporlles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootporllesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
