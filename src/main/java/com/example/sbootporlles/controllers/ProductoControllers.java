1228b187d1579a2d0a762822612478a387952704
jose781 <146405410+jfernando234@users.noreply.github.com>
2025-06-21 07:42:49 -0500

    feat: services de productos y proveedores

--- src/main/java/com/example/sbootporlles/controllers/ProductoControllers.java
+++ src/main/java/com/example/sbootporlles/controllers/ProductoControllers.java
@@ -2,12 +2,21 @@ package com.example.sbootporlles.controllers;
 
 import com.example.sbootporlles.models.ProductoModel;
 import com.example.sbootporlles.services.ProductoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @RestController
 @RequestMapping("/productos")
 public class ProductoControllers {
@@ -15,19 +24,21 @@ public class ProductoControllers {
     @Autowired
     private ProductoService productoService;
 
-    @GetMapping
+    @GetMapping("/GetAll")
     public List<ProductoModel> listarProductos() {
         return productoService.obtenerTodosProducto();
     }
 
-    @GetMapping("/{id}")
+    @PostMapping ("/{id}")
     public Optional<ProductoModel> obtenerProductoPorId(@PathVariable int id) {
         return productoService.obtenerPorId(id);
     }
 
-    @GetMapping("/crear")
-    public ProductoModel crearProducto(@RequestBody ProductoModel producto) {
-         System.out.println(producto.getNombre()+"::");
+    @PostMapping("/crear")
+    public ProductoModel crearProducto(
+        @RequestPart("producto") ProductoModel producto,
+        @RequestPart("imagen") MultipartFile imagen) throws IOException {
+        producto.setImagen(imagen.getBytes());
         return productoService.guardarProducto(producto);
     }
 
@@ -39,5 +50,17 @@ public class ProductoControllers {
     @DeleteMapping("/{id}")
     public void eliminarProducto(@PathVariable int id) {
         productoService.eliminarProducto(id);
+    }
+    @GetMapping("/imagen/{id}")
+    public ResponseEntity<byte[]> obtenerImagenProducto(@PathVariable int id) {
+        Optional<ProductoModel> productoOpt = productoService.obtenerPorId(id);
+        if (productoOpt.isPresent() && productoOpt.get().getImagen() != null) {
+            byte[] imagen = productoOpt.get().getImagen();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_JPEG); // O IMAGE_PNG según corresponda
+            return new ResponseEntity<>(imagen, headers, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
